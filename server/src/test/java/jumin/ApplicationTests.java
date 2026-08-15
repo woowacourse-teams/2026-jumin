4c01f3365d3bd59785e5a68abb1ce9093e560ee9
박병훈 <leafloveu@naver.com>
2026-08-15 12:55:43 +0900

    test(server): 테스트 컨텍스트 격리와 설명 정리

--- server/src/test/java/jumin/ApplicationTests.java
+++ server/src/test/java/jumin/ApplicationTests.java
@@ -1,6 +1,7 @@
 package jumin;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,6 +12,12 @@ import org.springframework.test.context.ActiveProfiles;
 class ApplicationTests {
 
     @Test
+    @DisplayName("애플리케이션 컨텍스트가 정상적으로 로드된다")
     void contextLoads() {
+        // when
+        // Spring Boot initializes the application context before the test.
+
+        // then
+        // Context initialization failure causes this test to fail.
     }
 }
