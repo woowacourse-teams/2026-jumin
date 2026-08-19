813f4e0d0d1e3ac28a2122d92172fb0db0be67c4
김상윤 <ksy151020@gmail.com>
2026-08-19 15:12:48 +0900

    feat(client): 최근 검색 목록 및 삭제 기능 추가

--- client/src/pages/SearchPage/SearchPage.tsx
+++ client/src/pages/SearchPage/SearchPage.tsx
@@ -1,10 +1,21 @@
 import { css } from '@emotion/css';
 import { useState } from 'react';
+import type { Destination } from '../../../api/contracts';
 import SearchBar from '../../../shared/components/SearchBar';
 import BottomNav from '../../../shared/components/BottomNav';
+import { RecentSearchList } from './components/RecentSearchList';
 
 const SearchPage = () => {
   const [query, setQuery] = useState('');
+  const [recentSearches, setRecentSearches] = useState<Destination[]>([]);
+
+  const selectRecentSearch = (destination: Destination) => {
+    setQuery(destination.name);
+  };
+
+  const removeRecentSearch = (destinationId: string) => {
+    setRecentSearches((current) => current.filter((destination) => destination.destinationId !== destinationId));
+  };
 
   return (
     <div
@@ -26,6 +37,14 @@ const SearchPage = () => {
         `}
       >
         <SearchBar autoFocus value={query} onChange={(event) => setQuery(event.target.value)} />
+
+        <RecentSearchList
+          recentSearches={recentSearches}
+          onSelect={selectRecentSearch}
+          onRemove={removeRecentSearch}
+          onClear={() => setRecentSearches([])}
+        />
+
         <footer
           className={css`
             margin-top: auto;
