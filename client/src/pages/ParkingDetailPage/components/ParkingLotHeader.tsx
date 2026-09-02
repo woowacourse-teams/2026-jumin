136db5c1f0e7fc39d1e63f88c098b13978f52ce1
김상윤 <ksy151020@gmail.com>
2026-09-02 19:41:35 +0900

    style(client): ParkingLotHeader 높이 조정

--- client/src/pages/ParkingDetailPage/components/ParkingLotHeader.tsx
+++ client/src/pages/ParkingDetailPage/components/ParkingLotHeader.tsx
@@ -42,9 +42,9 @@ const headerStyle = css`
 
   align-items: center;
 
-  height: calc(88px + env(safe-area-inset-top, 0px));
+  height: calc(60px + env(safe-area-inset-top, 0px));
 
-  padding: calc(24px + env(safe-area-inset-top, 0px)) 18px 0;
+  padding: calc(0px + env(safe-area-inset-top, 0px)) 18px 0;
 
   box-sizing: border-box;
 
